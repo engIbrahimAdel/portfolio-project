985b4c627cba0bd435b2039558214c0d8f967e26
engIbrahimAdel <ibrahim.zahra91@eng1.cu.edu.eg>
2018-04-25 06:36:24 +0200

    creating blog model

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'jobs.apps.JobsConfig',#this name jobs.config from apps in jobs
     'django.contrib.admin',
     'django.contrib.auth',
