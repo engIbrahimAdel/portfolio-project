42af379339c0bfeb90b7661276afc8501b618019
engIbrahimAdel <ibrahim.zahra91@eng1.cu.edu.eg>
2018-04-25 07:47:03 +0200

    making homepage

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,7 +18,9 @@ from django.urls import path
 #importing
 from django.conf import settings
 from django.conf.urls.static import static
+import jobs.views
 
 urlpatterns = [
+    path('',jobs.views.home,name='home'),
     path('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
